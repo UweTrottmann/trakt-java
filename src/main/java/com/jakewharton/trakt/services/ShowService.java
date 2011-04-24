5a062246b3ece73ec0fc8a932401730bf360faf9
Jake Wharton <jakewharton@gmail.com>
2011-04-24 00:54:16 -0400

    Add seen/watch/library list management methods.

--- src/main/java/com/jakewharton/trakt/services/ShowService.java
+++ src/main/java/com/jakewharton/trakt/services/ShowService.java
@@ -1,6 +1,8 @@
 package com.jakewharton.trakt.services;
 
 import java.util.List;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
 import com.google.gson.reflect.TypeToken;
 import com.jakewharton.trakt.TraktApiBuilder;
 import com.jakewharton.trakt.TraktApiService;
@@ -27,6 +29,586 @@ public class ShowService extends TraktApiService {
 		
 		private CancelWatchingBuilder(ShowService service) {
 			super(service, new TypeToken<Response>() {}, URI, HttpMethod.Post);
+		}
+	}
+	
+	/**
+	 * Add unwatched episodes to your library.
+	 * 
+	 * @param imdbId Show IMDB ID.
+	 * @return Builder instance.
+	 */
+	public LibraryBuilder library(String imdbId) {
+		return new LibraryBuilder(this).imdbId(imdbId);
+	}
+	
+	/**
+	 * Add unwatched episodes to your library.
+	 * 
+	 * @param tmdbId Show TMDB ID.
+	 * @return Builder instance.
+	 */
+	public LibraryBuilder library(int tmdbId) {
+		return new LibraryBuilder(this).tmdbId(tmdbId);
+	}
+	
+	/**
+	 * Add unwatched episodes to your library.
+	 * 
+	 * @param title Show title.
+	 * @param year Show year.
+	 * @return Builder instance.
+	 */
+	public LibraryBuilder library(String title, int year) {
+		return new LibraryBuilder(this).title(title).year(year);
+	}
+	
+	public static final class LibraryBuilder extends TraktApiBuilder<Void> {
+		private static final String POST_IMDB_ID = "imdb_id";
+		private static final String POST_TMDB_ID = "tmdb_id";
+		private static final String POST_TITLE = "title";
+		private static final String POST_YEAR = "year";
+		private static final String POST_EPISODES = "episodes";
+		private static final String POST_SEASON = "season";
+		private static final String POST_EPISODE = "episode";
+		
+		private static final String URI = "/show/episode/library/" + FIELD_API_KEY;
+		
+		private JsonArray episodeList;
+		
+		private LibraryBuilder(ShowService service) {
+			super(service, new TypeToken<Void>() {}, URI, HttpMethod.Post);
+			
+			this.episodeList = new JsonArray();
+		}
+		
+		/**
+		 * Show IMDB ID.
+		 * 
+		 * @param imdbId Value.
+		 * @return Builder instance.
+		 */
+		public LibraryBuilder imdbId(String imdbId) {
+			this.postParameter(POST_IMDB_ID, imdbId);
+			return this;
+		}
+		
+		/**
+		 * Show TMDB ID.
+		 * 
+		 * @param tmdbId Value.
+		 * @return Builder instance.
+		 */
+		public LibraryBuilder tmdbId(int tmdbId) {
+			this.postParameter(POST_TMDB_ID, tmdbId);
+			return this;
+		}
+		
+		/**
+		 * Show title.
+		 * 
+		 * @param title Value.
+		 * @return Builder instance.
+		 */
+		public LibraryBuilder title(String title) {
+			this.postParameter(POST_TITLE, title);
+			return this;
+		}
+		
+		/**
+		 * Show year.
+		 * 
+		 * @param year Value.
+		 * @return Builder instance.
+		 */
+		public LibraryBuilder year(int year) {
+			this.postParameter(POST_YEAR, year);
+			return this;
+		}
+		
+		/**
+		 * Add an episode to the list.
+		 * 
+		 * @param season Episode's season number.
+		 * @param episode Episode's number.
+		 * @return Builder instance.
+		 */
+		public LibraryBuilder episode(int season, int episode) {
+			JsonObject ep = new JsonObject();
+			ep.addProperty(POST_SEASON, season);
+			ep.addProperty(POST_EPISODE, episode);
+			this.episodeList.add(ep);
+			return this;
+		}
+
+		@Override
+		protected void preFireCallback() {
+			//Add the assembled movie list to the JSON post body.
+			this.postParameter(POST_EPISODES, this.episodeList);
+		}
+	}
+	
+	/**
+	 * Add episodes watched outside of trakt to your library.
+	 * 
+	 * @param imdbId Show IMDB ID.
+	 * @return Builder instance.
+	 */
+	public SeenBuilder seen(String imdbId) {
+		return new SeenBuilder(this).imdbId(imdbId);
+	}
+	
+	/**
+	 * Add episodes watched outside of trakt to your library.
+	 * 
+	 * @param tmdbId Show TMDB ID.
+	 * @return Builder instance.
+	 */
+	public SeenBuilder seen(int tmdbId) {
+		return new SeenBuilder(this).tmdbId(tmdbId);
+	}
+	
+	/**
+	 * Add episodes watched outside of trakt to your library.
+	 * 
+	 * @param title Show title.
+	 * @param year Show year.
+	 * @return Builder instance.
+	 */
+	public SeenBuilder seen(String title, int year) {
+		return new SeenBuilder(this).title(title).year(year);
+	}
+	
+	public static final class SeenBuilder extends TraktApiBuilder<Void> {
+		private static final String POST_IMDB_ID = "imdb_id";
+		private static final String POST_TMDB_ID = "tmdb_id";
+		private static final String POST_TITLE = "title";
+		private static final String POST_YEAR = "year";
+		private static final String POST_EPISODES = "episodes";
+		private static final String POST_SEASON = "season";
+		private static final String POST_EPISODE = "episode";
+		
+		private static final String URI = "/show/episode/seen/" + FIELD_API_KEY;
+		
+		private JsonArray episodeList;
+		
+		private SeenBuilder(ShowService service) {
+			super(service, new TypeToken<Void>() {}, URI, HttpMethod.Post);
+			
+			this.episodeList = new JsonArray();
+		}
+		
+		/**
+		 * Show IMDB ID.
+		 * 
+		 * @param imdbId Value.
+		 * @return Builder instance.
+		 */
+		public SeenBuilder imdbId(String imdbId) {
+			this.postParameter(POST_IMDB_ID, imdbId);
+			return this;
+		}
+		
+		/**
+		 * Show TMDB ID.
+		 * 
+		 * @param tmdbId Value.
+		 * @return Builder instance.
+		 */
+		public SeenBuilder tmdbId(int tmdbId) {
+			this.postParameter(POST_TMDB_ID, tmdbId);
+			return this;
+		}
+		
+		/**
+		 * Show title.
+		 * 
+		 * @param title Value.
+		 * @return Builder instance.
+		 */
+		public SeenBuilder title(String title) {
+			this.postParameter(POST_TITLE, title);
+			return this;
+		}
+		
+		/**
+		 * Show year.
+		 * 
+		 * @param year Value.
+		 * @return Builder instance.
+		 */
+		public SeenBuilder year(int year) {
+			this.postParameter(POST_YEAR, year);
+			return this;
+		}
+		
+		/**
+		 * Add an episode to the list.
+		 * 
+		 * @param season Episode's season number.
+		 * @param episode Episode's number.
+		 * @return Builder instance.
+		 */
+		public SeenBuilder episode(int season, int episode) {
+			JsonObject ep = new JsonObject();
+			ep.addProperty(POST_SEASON, season);
+			ep.addProperty(POST_EPISODE, episode);
+			this.episodeList.add(ep);
+			return this;
+		}
+
+		@Override
+		protected void preFireCallback() {
+			//Add the assembled movie list to the JSON post body.
+			this.postParameter(POST_EPISODES, this.episodeList);
+		}
+	}
+	
+	/**
+	 * Remove episodes from your library collection.
+	 * 
+	 * @param imdbId Show IMDB ID.
+	 * @return Builder instance.
+	 */
+	public UnlibraryBuilder unlibrary(String imdbId) {
+		return new UnlibraryBuilder(this).imdbId(imdbId);
+	}
+	
+	/**
+	 * Remove episodes from your library collection.
+	 * 
+	 * @param tmdbId Show TMDB ID.
+	 * @return Builder instance.
+	 */
+	public UnlibraryBuilder unlibrary(int tmdbId) {
+		return new UnlibraryBuilder(this).tmdbId(tmdbId);
+	}
+	
+	/**
+	 * Remove episodes from your library collection.
+	 * 
+	 * @param title Show title.
+	 * @param year Show year.
+	 * @return Builder instance.
+	 */
+	public UnlibraryBuilder unlibrary(String title, int year) {
+		return new UnlibraryBuilder(this).title(title).year(year);
+	}
+	
+	public static final class UnlibraryBuilder extends TraktApiBuilder<Void> {
+		private static final String POST_IMDB_ID = "imdb_id";
+		private static final String POST_TMDB_ID = "tmdb_id";
+		private static final String POST_TITLE = "title";
+		private static final String POST_YEAR = "year";
+		private static final String POST_EPISODES = "episodes";
+		private static final String POST_SEASON = "season";
+		private static final String POST_EPISODE = "episode";
+		
+		private static final String URI = "/show/episode/unlibrary/" + FIELD_API_KEY;
+		
+		private JsonArray episodeList;
+		
+		private UnlibraryBuilder(ShowService service) {
+			super(service, new TypeToken<Void>() {}, URI, HttpMethod.Post);
+			
+			this.episodeList = new JsonArray();
+		}
+		
+		/**
+		 * Show IMDB ID.
+		 * 
+		 * @param imdbId Value.
+		 * @return Builder instance.
+		 */
+		public UnlibraryBuilder imdbId(String imdbId) {
+			this.postParameter(POST_IMDB_ID, imdbId);
+			return this;
+		}
+		
+		/**
+		 * Show TMDB ID.
+		 * 
+		 * @param tmdbId Value.
+		 * @return Builder instance.
+		 */
+		public UnlibraryBuilder tmdbId(int tmdbId) {
+			this.postParameter(POST_TMDB_ID, tmdbId);
+			return this;
+		}
+		
+		/**
+		 * Show title.
+		 * 
+		 * @param title Value.
+		 * @return Builder instance.
+		 */
+		public UnlibraryBuilder title(String title) {
+			this.postParameter(POST_TITLE, title);
+			return this;
+		}
+		
+		/**
+		 * Show year.
+		 * 
+		 * @param year Value.
+		 * @return Builder instance.
+		 */
+		public UnlibraryBuilder year(int year) {
+			this.postParameter(POST_YEAR, year);
+			return this;
+		}
+		
+		/**
+		 * Add an episode to the list.
+		 * 
+		 * @param season Episode's season number.
+		 * @param episode Episode's number.
+		 * @return Builder instance.
+		 */
+		public UnlibraryBuilder episode(int season, int episode) {
+			JsonObject ep = new JsonObject();
+			ep.addProperty(POST_SEASON, season);
+			ep.addProperty(POST_EPISODE, episode);
+			this.episodeList.add(ep);
+			return this;
+		}
+
+		@Override
+		protected void preFireCallback() {
+			//Add the assembled movie list to the JSON post body.
+			this.postParameter(POST_EPISODES, this.episodeList);
+		}
+	}
+	
+	/**
+	 * Remove episodes watched outside of trakt from your library.
+	 * 
+	 * @param imdbId Show IMDB ID.
+	 * @return Builder instance.
+	 */
+	public UnseenBuilder unseen(String imdbId) {
+		return new UnseenBuilder(this).imdbId(imdbId);
+	}
+	
+	/**
+	 * Remove episodes watched outside of trakt from your library.
+	 * 
+	 * @param tmdbId Show TMDB ID.
+	 * @return Builder instance.
+	 */
+	public UnseenBuilder unseen(int tmdbId) {
+		return new UnseenBuilder(this).tmdbId(tmdbId);
+	}
+	
+	/**
+	 * Remove episodes watched outside of trakt from your library.
+	 * 
+	 * @param title Show title.
+	 * @param year Show year.
+	 * @return Builder instance.
+	 */
+	public UnseenBuilder unseen(String title, int year) {
+		return new UnseenBuilder(this).title(title).year(year);
+	}
+	
+	public static final class UnseenBuilder extends TraktApiBuilder<Void> {
+		private static final String POST_IMDB_ID = "imdb_id";
+		private static final String POST_TMDB_ID = "tmdb_id";
+		private static final String POST_TITLE = "title";
+		private static final String POST_YEAR = "year";
+		private static final String POST_EPISODES = "episodes";
+		private static final String POST_SEASON = "season";
+		private static final String POST_EPISODE = "episode";
+		
+		private static final String URI = "/show/episode/unseen/" + FIELD_API_KEY;
+		
+		private JsonArray episodeList;
+		
+		private UnseenBuilder(ShowService service) {
+			super(service, new TypeToken<Void>() {}, URI, HttpMethod.Post);
+			
+			this.episodeList = new JsonArray();
+		}
+		
+		/**
+		 * Show IMDB ID.
+		 * 
+		 * @param imdbId Value.
+		 * @return Builder instance.
+		 */
+		public UnseenBuilder imdbId(String imdbId) {
+			this.postParameter(POST_IMDB_ID, imdbId);
+			return this;
+		}
+		
+		/**
+		 * Show TMDB ID.
+		 * 
+		 * @param tmdbId Value.
+		 * @return Builder instance.
+		 */
+		public UnseenBuilder tmdbId(int tmdbId) {
+			this.postParameter(POST_TMDB_ID, tmdbId);
+			return this;
+		}
+		
+		/**
+		 * Show title.
+		 * 
+		 * @param title Value.
+		 * @return Builder instance.
+		 */
+		public UnseenBuilder title(String title) {
+			this.postParameter(POST_TITLE, title);
+			return this;
+		}
+		
+		/**
+		 * Show year.
+		 * 
+		 * @param year Value.
+		 * @return Builder instance.
+		 */
+		public UnseenBuilder year(int year) {
+			this.postParameter(POST_YEAR, year);
+			return this;
+		}
+		
+		/**
+		 * Add an episode to the list.
+		 * 
+		 * @param season Episode's season number.
+		 * @param episode Episode's number.
+		 * @return Builder instance.
+		 */
+		public UnseenBuilder episode(int season, int episode) {
+			JsonObject ep = new JsonObject();
+			ep.addProperty(POST_SEASON, season);
+			ep.addProperty(POST_EPISODE, episode);
+			this.episodeList.add(ep);
+			return this;
+		}
+
+		@Override
+		protected void preFireCallback() {
+			//Add the assembled movie list to the JSON post body.
+			this.postParameter(POST_EPISODES, this.episodeList);
+		}
+	}
+	
+	/**
+	 * Remove one or more episodes for a specific show from your watchlist.
+	 * 
+	 * @param imdbId Show IMDB ID.
+	 * @return Builder instance.
+	 */
+	public UnwatchlistBuilder unwatchlist(String imdbId) {
+		return new UnwatchlistBuilder(this).imdbId(imdbId);
+	}
+	
+	/**
+	 * Remove one or more episodes for a specific show from your watchlist.
+	 * 
+	 * @param tmdbId Show TMDB ID.
+	 * @return Builder instance.
+	 */
+	public UnwatchlistBuilder unwatchlist(int tmdbId) {
+		return new UnwatchlistBuilder(this).tmdbId(tmdbId);
+	}
+	
+	/**
+	 * Remove one or more episodes for a specific show from your watchlist.
+	 * 
+	 * @param title Show title.
+	 * @param year Show year.
+	 * @return Builder instance.
+	 */
+	public UnwatchlistBuilder unwatchlist(String title, int year) {
+		return new UnwatchlistBuilder(this).title(title).year(year);
+	}
+	
+	public static final class UnwatchlistBuilder extends TraktApiBuilder<Void> {
+		private static final String POST_IMDB_ID = "imdb_id";
+		private static final String POST_TMDB_ID = "tmdb_id";
+		private static final String POST_TITLE = "title";
+		private static final String POST_YEAR = "year";
+		private static final String POST_EPISODES = "episodes";
+		private static final String POST_SEASON = "season";
+		private static final String POST_EPISODE = "episode";
+		
+		private static final String URI = "/show/episode/unwatchlist/" + FIELD_API_KEY;
+		
+		private JsonArray episodeList;
+		
+		private UnwatchlistBuilder(ShowService service) {
+			super(service, new TypeToken<Void>() {}, URI, HttpMethod.Post);
+			
+			this.episodeList = new JsonArray();
+		}
+		
+		/**
+		 * Show IMDB ID.
+		 * 
+		 * @param imdbId Value.
+		 * @return Builder instance.
+		 */
+		public UnwatchlistBuilder imdbId(String imdbId) {
+			this.postParameter(POST_IMDB_ID, imdbId);
+			return this;
+		}
+		
+		/**
+		 * Show TMDB ID.
+		 * 
+		 * @param tmdbId Value.
+		 * @return Builder instance.
+		 */
+		public UnwatchlistBuilder tmdbId(int tmdbId) {
+			this.postParameter(POST_TMDB_ID, tmdbId);
+			return this;
+		}
+		
+		/**
+		 * Show title.
+		 * 
+		 * @param title Value.
+		 * @return Builder instance.
+		 */
+		public UnwatchlistBuilder title(String title) {
+			this.postParameter(POST_TITLE, title);
+			return this;
+		}
+		
+		/**
+		 * Show year.
+		 * 
+		 * @param year Value.
+		 * @return Builder instance.
+		 */
+		public UnwatchlistBuilder year(int year) {
+			this.postParameter(POST_YEAR, year);
+			return this;
+		}
+		
+		/**
+		 * Add an episode to the list.
+		 * 
+		 * @param season Episode's season number.
+		 * @param episode Episode's number.
+		 * @return Builder instance.
+		 */
+		public UnwatchlistBuilder episode(int season, int episode) {
+			JsonObject ep = new JsonObject();
+			ep.addProperty(POST_SEASON, season);
+			ep.addProperty(POST_EPISODE, episode);
+			this.episodeList.add(ep);
+			return this;
+		}
+
+		@Override
+		protected void preFireCallback() {
+			//Add the assembled movie list to the JSON post body.
+			this.postParameter(POST_EPISODES, this.episodeList);
 		}
 	}
 	
@@ -63,6 +645,122 @@ public class ShowService extends TraktApiService {
 			this.field(FIELD_EPISODE, episode);
 			
 			return this;
+		}
+	}
+	
+	/**
+	 * Add one or more episodes for a specific show to your watchlist.
+	 * 
+	 * @param imdbId Show IMDB ID.
+	 * @return Builder instance.
+	 */
+	public WatchlistBuilder watchlist(String imdbId) {
+		return new WatchlistBuilder(this).imdbId(imdbId);
+	}
+	
+	/**
+	 * Add one or more episodes for a specific show to your watchlist.
+	 * 
+	 * @param tmdbId Show TMDB ID.
+	 * @return Builder instance.
+	 */
+	public WatchlistBuilder watchlist(int tmdbId) {
+		return new WatchlistBuilder(this).tmdbId(tmdbId);
+	}
+	
+	/**
+	 * Add one or more episodes for a specific show to your watchlist.
+	 * 
+	 * @param title Show title.
+	 * @param year Show year.
+	 * @return Builder instance.
+	 */
+	public WatchlistBuilder watchlist(String title, int year) {
+		return new WatchlistBuilder(this).title(title).year(year);
+	}
+	
+	public static final class WatchlistBuilder extends TraktApiBuilder<Void> {
+		private static final String POST_IMDB_ID = "imdb_id";
+		private static final String POST_TMDB_ID = "tmdb_id";
+		private static final String POST_TITLE = "title";
+		private static final String POST_YEAR = "year";
+		private static final String POST_EPISODES = "episodes";
+		private static final String POST_SEASON = "season";
+		private static final String POST_EPISODE = "episode";
+		
+		private static final String URI = "/show/episode/watchlist/" + FIELD_API_KEY;
+		
+		private JsonArray episodeList;
+		
+		private WatchlistBuilder(ShowService service) {
+			super(service, new TypeToken<Void>() {}, URI, HttpMethod.Post);
+			
+			this.episodeList = new JsonArray();
+		}
+		
+		/**
+		 * Show IMDB ID.
+		 * 
+		 * @param imdbId Value.
+		 * @return Builder instance.
+		 */
+		public WatchlistBuilder imdbId(String imdbId) {
+			this.postParameter(POST_IMDB_ID, imdbId);
+			return this;
+		}
+		
+		/**
+		 * Show TMDB ID.
+		 * 
+		 * @param tmdbId Value.
+		 * @return Builder instance.
+		 */
+		public WatchlistBuilder tmdbId(int tmdbId) {
+			this.postParameter(POST_TMDB_ID, tmdbId);
+			return this;
+		}
+		
+		/**
+		 * Show title.
+		 * 
+		 * @param title Value.
+		 * @return Builder instance.
+		 */
+		public WatchlistBuilder title(String title) {
+			this.postParameter(POST_TITLE, title);
+			return this;
+		}
+		
+		/**
+		 * Show year.
+		 * 
+		 * @param year Value.
+		 * @return Builder instance.
+		 */
+		public WatchlistBuilder year(int year) {
+			this.postParameter(POST_YEAR, year);
+			return this;
+		}
+		
+		/**
+		 * Add an episode to the list.
+		 * 
+		 * @param season Episode's season number.
+		 * @param episode Episode's number.
+		 * @return Builder instance.
+		 */
+		public WatchlistBuilder episode(int season, int episode) {
+			JsonObject ep = new JsonObject();
+			ep.addProperty(POST_SEASON, season);
+			ep.addProperty(POST_EPISODE, episode);
+			this.episodeList.add(ep);
+			return this;
+		}
+
+		@Override
+		protected void preFireCallback() {
+			//Add the assembled movie list to the JSON post body.
+			this.postParameter(POST_EPISODES, this.episodeList);
 		}
 	}
 	
