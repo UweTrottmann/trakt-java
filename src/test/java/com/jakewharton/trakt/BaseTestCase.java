3bd0b1099eadbc3a983a89f1762a42629af48257
Jake Wharton <jakewharton@gmail.com>
2011-07-02 00:04:59 -0400

    Add authentication for our test user.

--- src/test/java/com/jakewharton/trakt/BaseTestCase.java
+++ src/test/java/com/jakewharton/trakt/BaseTestCase.java
@@ -8,6 +8,7 @@ public abstract class BaseTestCase extends TestCase {
 	@Override
 	public void setUp() {
 		manager.setApiKey("de8bc5e9866c77d655310bb0c8ab9a83");
+		manager.setAuthentication("trakt-java", "e00e0e31fe07213b59e5784c9942cfe220771827");
 	}
 	
 	protected final ServiceManager getManager() {
