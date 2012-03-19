907a11d5e5c1875f1776d1e5172d84564f9e02c0
Florian <florian@BikiniBottom.(none)>
2012-03-19 22:23:07 +0100

    add spoiler attribute

--- src/main/java/com/jakewharton/trakt/entities/Shout.java
+++ src/main/java/com/jakewharton/trakt/entities/Shout.java
@@ -9,6 +9,7 @@ public class Shout implements TraktEntity {
     public Calendar inserted;
     public String shout;
     public UserProfile user;
+    public Boolean spoiler;
 
     /** @deprecated Use {@link #inserted} */
     @Deprecated
