48ff6735dd682c0edda76a7ebdd18ba3d5b6f2db
Uwe Trottmann <uwe.trottmann@gmail.com>
2013-04-04 17:50:23 +0200

    Proactively do not encode separators for int and generic field arrays as well.

--- src/main/java/com/jakewharton/trakt/TraktApiBuilder.java
+++ src/main/java/com/jakewharton/trakt/TraktApiBuilder.java
@@ -321,7 +321,7 @@ public abstract class TraktApiBuilder<T> extends ApiBuilder {
                 builder.append(SEPERATOR);
             }
         }
-        return this.field(name, builder.toString());
+        return this.field(name, builder.toString(), false);
     }
 
     /**
@@ -339,7 +339,7 @@ public abstract class TraktApiBuilder<T> extends ApiBuilder {
                 builder.append(SEPERATOR);
             }
         }
-        return this.field(name, builder.toString());
+        return this.field(name, builder.toString(), false);
     }
 
     /**
