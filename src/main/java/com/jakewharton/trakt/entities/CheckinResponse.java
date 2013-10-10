a0f4dfc4d1283ea0c139e7f2ff1b051bccf981e2
Uwe Trottmann <uwe.trottmann@gmail.com>
2013-10-10 20:51:27 +0200

    Remove duplicate field.

--- src/main/java/com/jakewharton/trakt/entities/CheckinResponse.java
+++ src/main/java/com/jakewharton/trakt/entities/CheckinResponse.java
@@ -12,15 +12,7 @@ public class CheckinResponse extends Response {
         public int active_for;
     }
 
-    public static class Show implements TraktEntity {
-        public String title;
-        public int year;
-        public String imdb_id;
-        public int tvdb_id;
-    }
-
     public Timestamps timestamps;
-    public Show show;
     public boolean facebook;
     public boolean twitter;
     public boolean tumblr;
