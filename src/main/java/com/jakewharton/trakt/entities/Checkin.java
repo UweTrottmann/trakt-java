0a7037709f6d13c6253084977dce02b0c29cbb18
Uwe Trottmann <uwe.trottmann@gmail.com>
2013-10-10 20:58:46 +0200

    Add missing fields to Checkin, use objects to allow null values.

--- src/main/java/com/jakewharton/trakt/entities/Checkin.java
+++ src/main/java/com/jakewharton/trakt/entities/Checkin.java
@@ -5,16 +5,18 @@ import com.jakewharton.trakt.TraktEntity;
 public class Checkin implements TraktEntity {
 
     public String imdb_id;
-    public int tvdb_id;
+    public Integer tvdb_id;
     public String title;
-    public int year;
-    public int season;
-    public int episode;
-    public int duration;
-    public String app_version;
-    public String app_date;
-    public int venue_id;
+    public Integer year;
+    public Integer season;
+    public Integer episode;
+    public Integer episode_tvdb_id;
+    public Integer duration;
+    public Integer venue_id;
+    public String venue_name;
     public Share share;
     public String message;
+    public String app_version;
+    public String app_date;
 
 }
