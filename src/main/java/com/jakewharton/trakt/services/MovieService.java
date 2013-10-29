68081108c5f1cfa0fb205de1e51b3a98e9985294
Uwe Trottmann <uwe.trottmann@gmail.com>
2013-10-29 16:35:48 +0100

    Add movie/summary.

--- src/main/java/com/jakewharton/trakt/services/MovieService.java
+++ src/main/java/com/jakewharton/trakt/services/MovieService.java
@@ -3,7 +3,13 @@ package com.jakewharton.trakt.services;
 
 import com.jakewharton.trakt.entities.Response;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit.http.Body;
+import retrofit.http.GET;
 import retrofit.http.POST;
+import retrofit.http.Path;
 
 /**
  * Endpoints for Movie.
@@ -24,6 +30,21 @@ public interface MovieService {
     @POST("/movie/cancelwatching/{apikey}")
     Response cancelwatching();
 
+    @POST("/movie/seen/{apikey}")
+    void seen(
+            @Body Movies movies
+    );
+
+    @GET("/movie/summary.json/{apikey}/{title}")
+    com.jakewharton.trakt.entities.Movie summary(
+            @Path("title") String imdbIdOrSlug
+    );
+
+    @GET("/movie/summary.json/{apikey}/{title}")
+    com.jakewharton.trakt.entities.Movie summary(
+            @Path("title") int tmdbId
+    );
+
     public static class Movie {
 
         public String imdb_id;
@@ -34,5 +55,33 @@ public interface MovieService {
 
         public Integer year;
     }
+
+    public static class SeenMovie extends Movie {
+
+        public String last_played;
+
+        public SeenMovie(String imdbId) {
+            this.imdb_id = imdbId;
+        }
+
+        public SeenMovie(int tmdbId) {
+            this.tmdb_id = tmdbId;
+        }
+    }
+
+    public static class Movies {
+
+        public List<SeenMovie> movies;
+
+        public Movies(List<SeenMovie> seenMovies) {
+            movies = seenMovies;
+        }
+
+        public Movies(SeenMovie seenMovie) {
+            movies = new ArrayList<SeenMovie>();
+            movies.add(seenMovie);
+        }
+    }
+
 
 }
