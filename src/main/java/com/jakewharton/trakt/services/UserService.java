10bd0c39af390a16c7745a277509fb8ede7a0e62
Uwe Trottmann <uwe.trottmann@gmail.com>
2013-10-29 14:40:11 +0100

    Add user/network/friends, /followers and /following.

--- src/main/java/com/jakewharton/trakt/services/UserService.java
+++ src/main/java/com/jakewharton/trakt/services/UserService.java
@@ -3,20 +3,52 @@ package com.jakewharton.trakt.services;
 
 import com.jakewharton.trakt.entities.Movie;
 import com.jakewharton.trakt.entities.TvShow;
-import retrofit.http.GET;
-import retrofit.http.Path;
+import com.jakewharton.trakt.entities.UserProfile;
 
 import java.util.List;
+
+import retrofit.http.GET;
+import retrofit.http.Path;
 
 public interface UserService {
 
     /**
-     * Returns all movies in a user's watchlist. Each movie will indicate when
-     * it was added to the watchlist. Protected users won't return any data
-     * unless you are friends.
+     * Returns an array of all followers including the since timestamp which is when the
+     * relationship began. Protected users won't return any data unless you are friends. Any friends
+     * of the main user that are protected won't display data either.
+     */
+    @GET("/user/network/followers.json/{apikey}/{username}")
+    List<UserProfile> followers(
+            @Path("username") String username
+    );
+
+    /**
+     * Returns an array of all user's they follow including the since timestamp which is when the
+     * relationship began. Protected users won't return any data unless you are friends. Any friends
+     * of the main user that are protected won't display data either.
+     */
+    @GET("/user/network/following.json/{apikey}/{username}")
+    List<UserProfile> following(
+            @Path("username") String username
+    );
+
+    /**
+     * Returns an array of the user's friends (a 2 way relationship where each user follows the
+     * other) including the since timestamp which is when the friendship began. Protected users
+     * won't return any data unless you are friends. Any friends of the main user that are protected
+     * won't display data either.
+     */
+    @GET("/user/network/friends.json/{apikey}/{username}")
+    List<UserProfile> friends(
+            @Path("username") String username
+    );
+
+    /**
+     * Returns all movies in a user's watchlist. Each movie will indicate when it was added to the
+     * watchlist. Protected users won't return any data unless you are friends.
      *
-     * @param username You can get a username by browsing the website and
-     *                 looking at the URL when on a profile page.
+     * @param username You can get a username by browsing the website and looking at the URL when on
+     *                 a profile page.
      */
     @GET("/user/watchlist/movies.json/{apikey}/{username}")
     List<Movie> watchlistMovies(
@@ -24,12 +56,11 @@ public interface UserService {
     );
 
     /**
-     * Returns all shows in a user's watchlist. Each show will indicate when it
-     * was added to the watchlist. Protected users won't return any data unless
-     * you are friends.
+     * Returns all shows in a user's watchlist. Each show will indicate when it was added to the
+     * watchlist. Protected users won't return any data unless you are friends.
      *
-     * @param username You can get a username by browsing the website and
-     *                 looking at the URL when on a profile page.
+     * @param username You can get a username by browsing the website and looking at the URL when on
+     *                 a profile page.
      */
     @GET("/user/watchlist/shows.json/{apikey}/{username}")
     List<TvShow> watchlistShows(
@@ -37,12 +68,11 @@ public interface UserService {
     );
 
     /**
-     * Returns all shows in a user's library. Each show will indicate how many
-     * plays it has. Protected users won't return any data unless you are
-     * friends.
+     * Returns all shows in a user's library. Each show will indicate how many plays it has.
+     * Protected users won't return any data unless you are friends.
      *
-     * @param username You can get a username by browsing the website and
-     *                 looking at the URL when on a profile page.
+     * @param username You can get a username by browsing the website and looking at the URL when on
+     *                 a profile page.
      */
     @GET("/user/library/shows/all.json/{apikey}/{username}")
     List<TvShow> libraryShowsAll(
@@ -50,11 +80,11 @@ public interface UserService {
     );
 
     /**
-     * Returns complete show info if set to true. Only send this if you really need
-     * the full dump as it doubles the data size being sent back.
+     * Returns complete show info if set to true. Only send this if you really need the full dump as
+     * it doubles the data size being sent back.
      *
-     * @param username You can get a username by browsing the website and
-     *                 looking at the URL when on a profile page.
+     * @param username You can get a username by browsing the website and looking at the URL when on
+     *                 a profile page.
      */
     @GET("/user/library/shows/all.json/{apikey}/{username}/extended")
     List<TvShow> libraryShowsAllExtended(
@@ -62,11 +92,11 @@ public interface UserService {
     );
 
     /**
-     * Returns only the minimal info (title, year, imdb_id, tvdb_id, tvrage_id, plays)
-     * required for media center syncing if set to min. This sends about half the data.
+     * Returns only the minimal info (title, year, imdb_id, tvdb_id, tvrage_id, plays) required for
+     * media center syncing if set to min. This sends about half the data.
      *
-     * @param username You can get a username by browsing the website and
-     *                 looking at the URL when on a profile page.
+     * @param username You can get a username by browsing the website and looking at the URL when on
+     *                 a profile page.
      */
     @GET("/user/library/shows/all.json/{apikey}/{username}/min")
     List<TvShow> libraryShowsAllMinimum(
