baf5d31a793946b00febbdac6a0785639ecb04ca
Uwe Trottmann <uwe.trottmann@gmail.com>
2014-08-18 14:15:24 +0200

    Refactor Shows tests, fix test show and movie.

--- src/test/java/com/uwetrottmann/trakt/v2/TestData.java
+++ src/test/java/com/uwetrottmann/trakt/v2/TestData.java
@@ -2,15 +2,20 @@ package com.uwetrottmann.trakt.v2;
 
 public interface TestData {
 
-    public String MOVIE_TITLE = "The Bourne Legacy";
-    public int MOVIE_TRAKT_ID = 28;
-    public String MOVIE_SLUG = "the-bourne-legacy-2012";
-    public String MOVIE_IMDB_ID = "tt1194173";
-    public int MOVIE_TMDB_ID = 49040;
-    public int MOVIE_YEAR = 2012;
+    public String MOVIE_TITLE = "TRON: Legacy";
+    public int MOVIE_TRAKT_ID = 343;
+    public String MOVIE_SLUG = "tron-legacy-2010";
+    public String MOVIE_IMDB_ID = "tt1104001";
+    public int MOVIE_TMDB_ID = 20526;
+    public int MOVIE_YEAR = 2010;
 
-    public int SHOW_TRAKT_ID = 6;
-    public int SHOW_TVDB_ID = 121361;
+    public String SHOW_TITLE = "Game of Thrones";
+    public int SHOW_TRAKT_ID = 353;
     public String SHOW_SLUG = "game-of-thrones";
+    public String SHOW_IMDB_ID = "tt0944947";
+    public int SHOW_TMDB_ID = 1399;
+    public int SHOW_TVDB_ID = 121361;
+    public int SHOW_TVRAGE_ID = 24493;
+    public int SHOW_YEAR = 2011;
 
 }
--- src/test/java/com/uwetrottmann/trakt/v2/services/ShowsTest.java
+++ src/test/java/com/uwetrottmann/trakt/v2/services/ShowsTest.java
@@ -12,17 +12,26 @@ public class ShowsTest extends BaseTestCase {
     @Test
     public void test_summary_slug() {
         Show show = getTrakt().shows().summary(TestData.SHOW_SLUG);
-        assertThat(show).isNotNull();
-        assertThat(show.ids).isNotNull();
-        assertThat(show.ids.slug).isEqualTo(TestData.SHOW_SLUG);
+        assertTestShow(show);
     }
 
     @Test
     public void test_summary_trakt_id() {
         Show show = getTrakt().shows().summary(TestData.SHOW_TRAKT_ID);
+        assertTestShow(show);
+    }
+
+    private void assertTestShow(Show show) {
         assertThat(show).isNotNull();
+        assertThat(show.title).isEqualTo(TestData.SHOW_TITLE);
+        assertThat(show.year).isEqualTo(TestData.SHOW_YEAR);
         assertThat(show.ids).isNotNull();
         assertThat(show.ids.trakt).isEqualTo(TestData.SHOW_TRAKT_ID);
+        assertThat(show.ids.slug).isEqualTo(TestData.SHOW_SLUG);
+        assertThat(show.ids.imdb).isEqualTo(TestData.SHOW_IMDB_ID);
+        assertThat(show.ids.tmdb).isEqualTo(TestData.SHOW_TMDB_ID);
+        assertThat(show.ids.tvdb).isEqualTo(TestData.SHOW_TVDB_ID);
+        assertThat(show.ids.tvrage).isEqualTo(TestData.SHOW_TVRAGE_ID);
     }
 
 }
