4afe320bf7042f224e73c4ffde3bb0d3accccdbe
Uwe Trottmann <uwe.trottmann@gmail.com>
2014-08-22 11:31:23 +0200

    Support GET/POST/DELETE for `/sync/watchlist`.

---
+++ src/main/java/com/uwetrottmann/trakt/v2/entities/WatchlistedMovie.java
@@ -0,0 +1,7 @@
+package com.uwetrottmann.trakt.v2.entities;
+
+public class WatchlistedMovie extends BaseWatchlistedEntity {
+
+    public Movie movie;
+
+}
---
+++ src/main/java/com/uwetrottmann/trakt/v2/entities/WatchlistedShow.java
@@ -0,0 +1,7 @@
+package com.uwetrottmann.trakt.v2.entities;
+
+public class WatchlistedShow extends BaseWatchlistedEntity {
+
+    public Show show;
+
+}
