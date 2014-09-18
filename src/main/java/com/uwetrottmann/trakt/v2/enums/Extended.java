8c6aa910b84ea7c31f72280218b08e88cdf5034c
Uwe Trottmann <uwe.trottmann@gmail.com>
2014-09-18 15:47:13 +0200

    Add docs to Extended flag.

--- src/main/java/com/uwetrottmann/trakt/v2/enums/Extended.java
+++ src/main/java/com/uwetrottmann/trakt/v2/enums/Extended.java
@@ -1,10 +1,19 @@
 package com.uwetrottmann.trakt.v2.enums;
 
+/**
+ * By default, all methods will return minimal info for movies, shows, episodes, and users. Minimal info is typically
+ * all you need to match locally cached items and includes the title, year, and ids. However, you can request different
+ * extended levels of information.
+ */
 public enum Extended implements TraktEnum {
 
+    /** Default. Returns enough info to match locally. */
     DEFAULT_MIN("min"),
+    /** Minimal info and all images. */
     IMAGES("images"),
+    /** Complete info for an item. */
     FULL("full"),
+    /** Complete info and all images. */
     FULLIMAGES("full,images");
 
     private final String value;
