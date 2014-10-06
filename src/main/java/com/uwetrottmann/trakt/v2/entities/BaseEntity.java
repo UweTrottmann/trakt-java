441145c74f51428568f3b44cce72df8b070d25bf
Uwe Trottmann <uwe.trottmann@gmail.com>
2014-10-06 09:58:13 +0200

    Add updated_at property to base entity.

--- src/main/java/com/uwetrottmann/trakt/v2/entities/BaseEntity.java
+++ src/main/java/com/uwetrottmann/trakt/v2/entities/BaseEntity.java
@@ -1,10 +1,13 @@
 package com.uwetrottmann.trakt.v2.entities;
 
+import org.joda.time.DateTime;
+
 import java.util.List;
 
 public abstract class BaseEntity {
 
     public String title;
+    public DateTime updated_at;
     public Images images;
     public List<String> available_translations;
 
