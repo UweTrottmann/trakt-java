0515fe116bfdbed6e45e53c5e8298cb5e65b9d1b
Uwe Trottmann <uwe.trottmann@gmail.com>
2014-10-14 17:15:04 +0200

    Add missing properties to Movie.

--- src/main/java/com/uwetrottmann/trakt/v2/entities/Movie.java
+++ src/main/java/com/uwetrottmann/trakt/v2/entities/Movie.java
@@ -2,6 +2,8 @@ package com.uwetrottmann.trakt.v2.entities;
 
 import org.joda.time.DateTime;
 
+import java.util.List;
+
 public class Movie extends BaseEntity {
 
     public Integer year;
@@ -12,6 +14,10 @@ public class Movie extends BaseEntity {
     public String overview;
     public DateTime released;
     public Integer runtime;
+    public String trailer;
+    public String homepage;
+    public Double rating;
     public String language;
+    public List<String> genres;
 
 }
