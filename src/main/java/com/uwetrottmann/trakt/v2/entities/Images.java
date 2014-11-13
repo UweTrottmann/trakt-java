3440206c22b829742b235cd406bd62dd3f702d1e
Uwe Trottmann <uwe.trottmann@gmail.com>
2014-11-13 15:52:07 +0100

    Add additional image sizes for screenshots.

--- src/main/java/com/uwetrottmann/trakt/v2/entities/Images.java
+++ src/main/java/com/uwetrottmann/trakt/v2/entities/Images.java
@@ -4,13 +4,13 @@ public class Images {
 
     public MoreImageSizes poster;
     public MoreImageSizes fanart;
+    public MoreImageSizes screenshot;
     /** A {@link com.uwetrottmann.trakt.v2.entities.Person} has headshots. */
     public MoreImageSizes headshot;
     public ImageSizes banner;
     public ImageSizes logo;
     public ImageSizes clearart;
     public ImageSizes thumb;
-    public ImageSizes screenshot;
     public ImageSizes avatar;
 
 }
