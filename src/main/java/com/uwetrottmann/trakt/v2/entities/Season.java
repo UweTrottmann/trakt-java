d7947d36525366a9e8f13fbdab447be6a79f37ab
Uwe Trottmann <uwe.trottmann@gmail.com>
2014-11-13 15:48:37 +0100

    Add rating, episode_count, images to Season entity.

--- src/main/java/com/uwetrottmann/trakt/v2/entities/Season.java
+++ src/main/java/com/uwetrottmann/trakt/v2/entities/Season.java
@@ -1,8 +1,14 @@
 package com.uwetrottmann.trakt.v2.entities;
 
+import com.uwetrottmann.trakt.v2.enums.Rating;
+
 public class Season {
 
     public Integer number;
     public SeasonIds ids;
+
+    public Rating rating;
+    public Integer episode_count;
+    public Images images;
 
 }
