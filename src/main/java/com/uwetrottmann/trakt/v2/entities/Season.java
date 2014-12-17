031265529e44536c869afd9fc2cacf65784433cb
Uwe Trottmann <uwe.trottmann@gmail.com>
2014-12-17 13:57:04 +0100

    Make season rating a global rating, so double type.

--- src/main/java/com/uwetrottmann/trakt/v2/entities/Season.java
+++ src/main/java/com/uwetrottmann/trakt/v2/entities/Season.java
@@ -1,13 +1,11 @@
 package com.uwetrottmann.trakt.v2.entities;
 
-import com.uwetrottmann.trakt.v2.enums.Rating;
-
 public class Season {
 
     public Integer number;
     public SeasonIds ids;
 
-    public Rating rating;
+    public Double rating;
     public Integer episode_count;
     public Images images;
 
