5b267cb200d74676858c3ea923be3972f8863440
Sponge Bob <florian.pub@gmail.com>
2015-02-06 17:18:15 +0100

    add missing attributes

--- src/main/java/com/uwetrottmann/trakt/v2/entities/LastActivities.java
+++ src/main/java/com/uwetrottmann/trakt/v2/entities/LastActivities.java
@@ -1,7 +1,10 @@
 package com.uwetrottmann.trakt.v2.entities;
 
+import org.joda.time.DateTime;
+
 public class LastActivities {
 
+    public DateTime all;
     public LastActivityMore movies;
     public LastActivityMore episodes;
     public LastActivity shows;
