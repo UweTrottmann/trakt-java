9df09f0fff4449bfc113078da4dde1cb014e507e
Uwe Trottmann <uwe.trottmann@gmail.com>
2015-03-10 17:56:27 +0100

    Add aired_episodes property to Season.
    - Do some proper tests on Season entity.

--- src/test/java/com/uwetrottmann/trakt/v2/services/SeasonsTest.java
+++ src/test/java/com/uwetrottmann/trakt/v2/services/SeasonsTest.java
@@ -17,12 +17,29 @@ public class SeasonsTest extends BaseTestCase {
     @Test
     public void test_summary() {
         List<Season> seasons = getTrakt().seasons().summary(TestData.SHOW_SLUG, Extended.FULLIMAGES);
-        assertThat(seasons).isNotEmpty();
+        assertThat(seasons).hasSize(6);
+        for (Season season : seasons) {
+            assertThat(season).isNotNull();
+            // must have at least trakt and tvdb id
+            assertThat(season.ids.trakt).isPositive();
+            assertThat(season.ids.tvdb).isPositive();
+            // 5 seasons + sepcials for Breaking Bad
+            assertThat(season.number).isBetween(0, 5);
+            assertThat(season.episode_count).isPositive();
+            assertThat(season.aired_episodes).isPositive();
+            assertThat(season.rating).isBetween(0.0, 10.0);
+            assertThat(season.votes).isPositive();
+            // Breaking Bad seasons all have a poster
+            assertThat(season.images.poster.full).isNotNull();
+            assertThat(season.images.poster.medium).isNotNull();
+            assertThat(season.images.poster.thumb).isNotNull();
+        }
     }
 
     @Test
     public void test_season() {
-        List<Episode> season = getTrakt().seasons().season(TestData.SHOW_SLUG, TestData.EPISODE_SEASON, Extended.DEFAULT_MIN);
+        List<Episode> season = getTrakt().seasons().season(TestData.SHOW_SLUG, TestData.EPISODE_SEASON,
+                Extended.DEFAULT_MIN);
         assertThat(season).isNotEmpty();
         for (Episode episode : season) {
             assertThat(episode.season).isEqualTo(TestData.EPISODE_SEASON);
