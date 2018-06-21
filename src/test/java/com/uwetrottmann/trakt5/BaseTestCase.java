4046c973cc65787abbcab9f7d742a20f44cbcd6d
Uwe Trottmann <uwe+git@uwetrottmann.com>
2018-06-21 08:17:11 +0200

    Update test credentials.

--- src/test/java/com/uwetrottmann/trakt5/BaseTestCase.java
+++ src/test/java/com/uwetrottmann/trakt5/BaseTestCase.java
@@ -29,7 +29,7 @@ import static org.assertj.core.api.Assertions.fail;
 public class BaseTestCase {
 
     protected static final String TEST_CLIENT_ID = "35a671df22d3d98b09aab1c0bc52977e902e696a7704cab94f4d12c2672041e4";
-    public static final String TEST_ACCESS_TOKEN = "d797342dd08bbece0d3fecb559d3b0d19ae298492e3f09f0856dbdb7470babf7"; // "sgtest" on production
+    public static final String TEST_ACCESS_TOKEN = "510deb1c9058359f4932be362f20164e821faf003f65cc9738e0715087a6602c"; // "sgtest" on production
 
     private static final boolean DEBUG = true;
 
