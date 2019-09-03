aec2a4b709aac7319e6f91c27348074673a51d98
Marc de Courville <marc@courville.org>
2019-09-03 15:59:00 +0200

    Add scrobble and getPlayback sync capabilities.

---
+++ src/main/java/com/uwetrottmann/trakt5/entities/EpisodeProgress.java
@@ -0,0 +1,6 @@
+package com.uwetrottmann.trakt5.entities;
+
+public class EpisodeProgress extends Progress {
+    public SyncEpisode episode;
+    public String type = "episode";
+}
---
+++ src/main/java/com/uwetrottmann/trakt5/entities/MovieProgress.java
@@ -0,0 +1,6 @@
+package com.uwetrottmann.trakt5.entities;
+
+public class MovieProgress extends Progress {
+    public SyncMovie movie;
+    public String type = "movie";
+}
