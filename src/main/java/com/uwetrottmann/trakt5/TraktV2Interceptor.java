dc8cadc24085e542459d4da93cd59b319c2cd685
srggsf <dr.g100k@gmail.com>
2020-09-08 16:33:30 +0300

    92 - Staging environment support. - Code review fixes

--- src/main/java/com/uwetrottmann/trakt5/TraktV2Interceptor.java
+++ src/main/java/com/uwetrottmann/trakt5/TraktV2Interceptor.java
@@ -27,8 +27,9 @@ public class TraktV2Interceptor implements Interceptor {
     }
 
     /**
-     * If the host matches {@link TraktV2#API_HOST} adds a header for the current {@link TraktV2#API_VERSION}, {@link
-     * TraktV2#HEADER_TRAKT_API_KEY} with the given api key, {@link TraktV2#HEADER_CONTENT_TYPE} and if not present an
+     * If the host matches {@link TraktV2#API_HOST} or {@link TraktV2#API_STAGING_HOST} adds a header for
+     * the current {@link TraktV2#API_VERSION}, {@link TraktV2#HEADER_TRAKT_API_KEY} with the given api key,
+     * {@link TraktV2#HEADER_CONTENT_TYPE} and if not present an
      * Authorization header using the given access token.
      *
      * If a request fails due to HTTP 429 Too Many Requests, will retry the request after the time in seconds given
