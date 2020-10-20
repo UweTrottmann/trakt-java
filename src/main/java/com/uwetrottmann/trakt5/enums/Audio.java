9f165f9d74334c57cbb4f525507e8726bb8a3b7b
Rik Smith <rik.smith@ns.nl>
2020-10-20 16:37:39 +0200

    Add missing Audio types

--- src/main/java/com/uwetrottmann/trakt5/enums/Audio.java
+++ src/main/java/com/uwetrottmann/trakt5/enums/Audio.java
@@ -7,9 +7,12 @@ public enum Audio implements TraktEnum {
 
     LPCM("lpcm"),
     MP3("mp3"),
+    MP2("mp2"),
     AAC("aac"),
     OGG("ogg"),
+    OGG_OPUS("ogg_opus"),
     WMA("wma"),
+    FLAC("flac"),
     DTS("dts"),
     DTS_MA("dts_ma"),
     DTS_HR("dts_hr"),
@@ -17,6 +20,7 @@ public enum Audio implements TraktEnum {
     AURO_3D("auro_3d"),
     DOLBY_DIGITAL("dolby_digital"),
     DOLBY_DIGITAL_PLUS("dolby_digital_plus"),
+    DOLBY_DIGITAL_PLUS_ATMOS("dolby_digital_plus_atmos"),
     DOLBY_ATMOS("dolby_atmos"),
     DOLBY_TRUEHD("dolby_truehd"),
     DOLBY_PROLOGIC("dolby_prologic");
