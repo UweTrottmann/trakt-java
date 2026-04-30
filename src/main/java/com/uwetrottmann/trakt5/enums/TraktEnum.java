dd3919f5d79ec18f93b94a353d6044b1c6dc6bfd
Uwe Trottmann <uwe+git@uwetrottmann.com>
2026-04-30 05:54:11 +0200

    TraktEnum: add warning about serialization to class docs

--- src/main/java/com/uwetrottmann/trakt5/enums/TraktEnum.java
+++ src/main/java/com/uwetrottmann/trakt5/enums/TraktEnum.java
@@ -16,10 +16,11 @@
 
 package com.uwetrottmann.trakt5.enums;
 
+/**
+ * Warning: GSON does not use {@link #toString()} to serialize enums, but the key value.
+ * Use @SerializedName for these enums (or register both serializer and deserializer in TraktV2Helper).
+ */
 public interface TraktEnum {
-
-    // Note: GSON does not use .toString() to serialize enums, but the key value.
-    // Use @SerializedName for these enums (or register both serializer and deserializer in TraktV2Helper).
 
     /**
      * Return the value to be used by retrofit when building a request.
